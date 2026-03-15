cdb977e0dbd409096abe47cff692cb2473c56b76
Feroze Attai <ferozeattai@gmail.com>
2026-03-15 02:43:10 -0500

    Time: 1 ms (97.43%) | Memory: 42.8 MB (37.46%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x<2) return x;
+
+        long left  = 2, right = x/2;
+        long pivot;
+        long num;
+
+        while (left <= right){
+            pivot = left + (right - left ) / 2;
+            num = pivot * pivot;
+
+            if (num > x) right = pivot - 1;
+            else if ( num < x) left = pivot +1;
+            else return (int)pivot;
+        }
+        return (int) right;
+    } 
+}
