fa7027d09ad8eeb3ce3bdc4da8a3060e0c3323ba
Feroze Attai <ferozeattai@gmail.com>
2026-03-15 02:45:49 -0500

    Time: 20 ms (27.70%) | Memory: 94.1 MB (47.34%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.c
@@ -0,0 +1,29 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize) {
+    if(numsSize == 0 || k == 0) {
+        *returnSize = 0;
+        return NULL;
+    }
+    int* result = (int*)malloc(sizeof(int)* (numsSize - k +1 )); 
+    int* deque = (int*)malloc(sizeof(int)* numsSize);
+    int head = 0;
+    int tail = 0;
+    int resindex = 0;
+    for(int i = 0; i < numsSize ; i++){
+        if(head < tail && deque[head] <= i-k){
+            head++;
+        }
+        while(head < tail && nums[deque[tail-1]] <= nums[i]){
+            tail--;
+        }
+        deque[tail++]=i;
+        if(i >= k-1){
+            result[resindex++] = nums[deque[head]];
+            }
+    }
+    *returnSize = resindex;
+    free(deque);
+    return result;
+}
